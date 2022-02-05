a67664341b245964c7c37b5a724e11287446fa24
joe-main <qmmplusfact@gmail.com>
2022-02-05 20:02:41 +0900

    SSG/SSRの検証

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,27 @@
+
+import Link from 'next/link';
+import Head from 'next/head';
+const products = [{ name: 'bag' }, { name: 'shoes' }, { name: 'socks' }];
 export default function Home() {
-  return <h1>Hello Next.js</h1>;
+  return (
+    <div>
+      <Head>
+        <title>{products[1].name}</title>
+        <meta name="description" content={`${products[1].name}のページ`} />
+        <meta property="og:title" content={products[1].name} />
+        <meta
+          property="og:description"
+          content={`${products[1].name}のページ`}
+        />
+      </Head>
+      <ul>
+        <li>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </li>
+      </ul>
+      <h1>Hello Next.js</h1>
+    </div>
+  );
 }

5c8822a4c17406b808f21ebb1fd04f95dbc85de0
joe-main <qmmplusfact@gmail.com>
2022-02-07 20:12:21 +0900

    little fixes

--- pages/products/[name]/[color].js
+++ pages/products/[name]/[color].js
@@ -9,5 +9,6 @@ import { useRouter } from "next/router";
 export default function Color() {
   const router = useRouter();
   const { name, color } = router.query
+  console.log(router.query)
   return <h1>{ name }の{ color }カラーです</h1>;
 }
